8b25f2139b5ae3419b3bfffbbc3631b8baab7140
Asp2591 <119059499+Asp2591@users.noreply.github.com>
2024-03-16 22:23:00 +0530

    Add files via upload
    Added js file

---
+++ script.js
@@ -0,0 +1,69 @@
+let userScore=0;
+let compScore=0;
+
+const choices=document.querySelectorAll('.choice')
+
+const msg=document.querySelector('#msg')
+const start=document.querySelector('.start')
+let scoreUpdate1=document.querySelector('#yourScore')
+let scoreUpdate2=document.querySelector('#compScore')
+
+const genCompChoice=()=>{
+    const options=['rock','paper','scissor'];
+    const indx=Math.floor(Math.random()*3);
+    return options[indx];
+}
+
+const playGame=(userChoice)=>{
+    console.log('User Choice :',userChoice)
+
+    let compChoice=genCompChoice()
+    console.log('Comp Choice :',compChoice)
+
+    result(userChoice,compChoice)
+    
+}
+
+const draw=()=>{ 
+        console.log('Draw')
+        msg.innerText="Draw the game !! Try again";
+        start.style.backgroundColor='yellow';
+        msg.style.color='black'
+
+        
+}
+
+const result=(userChoice,compChoice)=>{ 
+    if(userChoice===compChoice){
+        draw()
+    }
+    else if  ((userChoice==='rock' && compChoice==='paper')||(userChoice==='paper' && compChoice==='scissor')||(userChoice==='scissor' && compChoice==='rock')) {
+        compScore++;
+        scoreUpdate2.innerText=compScore
+        msg.innerText=`You lost the game !! ${compChoice} beats ${userChoice}`;
+        console.log('You lost ')
+        start.style.backgroundColor='red';
+        msg.style.color='black'
+
+    } 
+    else {
+        userScore++;
+        scoreUpdate1.innerText=userScore
+        msg.innerText=`You won the game !! ${userChoice} beats ${compChoice}`;
+        console.log('You Won')
+        start.style.backgroundColor='green';
+        msg.style.color='white'
+
+
+    } 
+};
+
+
+
+choices.forEach((choice) => {
+    choice.addEventListener('click',()=>{
+        let userChoice=choice.getAttribute('id')
+        playGame(userChoice)
+        
+    })
+});
